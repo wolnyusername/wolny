006ccc5319232937246252143c5e6c0cf4ac6310
Michał <siny2@op.pl>
2022-09-20 16:38:50 +0200

    ..

--- shift/management/commands/endshift.py
+++ shift/management/commands/endshift.py
@@ -10,5 +10,5 @@ class Command(BaseCommand):
         for i in Shift.objects.filter(end_time=None):
             max_shift = datetime.timedelta(minutes=2)
             start_shift = Shift.objects.get(id=i.id).start_time
-            if datetime.datetime.now(datetime.timezone.utc) - start_shift >= max_shift:
+            if (datetime.datetime.now(datetime.timezone.utc) - start_shift) >= max_shift:
                 Shift.objects.filter(id=i.id).update(end_time=(start_shift+max_shift))
