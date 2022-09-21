919b3313df6a465a12468ecd4d93523f764cde3d
Michał <siny2@op.pl>
2022-09-21 15:20:08 +0200

    ...

--- shift/urls.py
+++ shift/urls.py
@@ -4,6 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('home', views.home, name='home'),
-    path('start', views.user_start_shift, name='user_start_shift'),
-    path('end', views.user_end_shift, name='user_end_shift'),
+    path('home/start', views.user_start_shift, name='user_start_shift'),
+    path('home/end', views.user_end_shift, name='user_end_shift'),
 ]
--- shift/views.py
+++ shift/views.py
@@ -7,7 +7,6 @@ from .models import Shift, Worker
 def index(request):
     #from pdb import set_trace;set_trace()
     context = {}
-    request.session.flush()
     if request.method == 'POST':
         user = models.Worker.objects.filter(login=request.POST.get('login'))
         if len(user) == 0:
@@ -15,47 +14,46 @@ def index(request):
         else:
             if user[0].password == request.POST.get('password'):
                 request.session['user'] = user[0].pk
-                user_shifts = list(Shift.objects.filter(worker=request.session.get('user'), end_time=None))
-                if len(user_shifts) == 0:
-                    return redirect('user_start_shift')
-                else:
-                    return redirect('user_end_shift')
+                request.session['logged'] = True
+                return redirect('home')
             else:
                 context['wrong_password'] = True
     return render(request, 'shift/index.html', context=context)
 
 def home(request):
     user_shifts = list(Shift.objects.filter(worker=request.session.get('user'), end_time=None))
-    if len(user_shifts) == 0:
-        return redirect('user_start_shift')
+    context = {}
+    if request.session['logged'] is True:
+        if len(user_shifts) == 0:
+            context['shift_started'] = False
+        else:
+            context['shift_started'] = True
+        if context['shift_started'] is False:
+            return redirect('user_start_shift')
+        else:
+            return redirect('user_end_shift')
     else:
-        return redirect('user_end_shift')
+        return redirect('index')
 
 def user_start_shift(request):
-    context = {}
-    user_shifts = list(Shift.objects.filter(worker=request.session.get('user'), end_time=None))
     user_id = request.session.get('user')
-    if len(user_shifts) == 0:
-        context['shift_not_started'] = True
-        if request.method == 'POST':
+    if request.method == 'POST':
+        if request.POST.get('start') == 'start':
             new_shift = Shift(worker=Worker(id=user_id))
             new_shift.save()
             return redirect('home')
         if request.POST.get('logout') == 'logout':
             request.session.flush()
             return redirect('index')
-    return render(request, 'shift/home.html', context=context)
+    return render(request, 'shift/home.html')
 
 def user_end_shift(request):
-    context = {}
-    user_shifts = list(Shift.objects.filter(worker=request.session.get('user'), end_time=None))
     user_id = request.session.get('user')
-    if len(user_shifts) > 0:
-        context['shift_already_started'] = True
-        if request.method == 'POST':
+    if request.method == 'POST':
+        if request.POST.get('end') == 'end':
             Shift.objects.filter(worker=Worker(id=user_id), end_time=None).update(end_time=datetime.datetime.now())
             return redirect('home')
         if request.POST.get('logout') == 'logout':
             request.session.flush()
             return redirect('index')
-    return render(request, 'shift/home.html', context=context)
+    return render(request, 'shift/home.html')
