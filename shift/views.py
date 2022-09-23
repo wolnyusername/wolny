644f8face107446fa458186023e8ad1cac72e0e8
Michał <siny2@op.pl>
2022-09-23 15:09:36 +0200

    klasa Context

--- shift/views.py
+++ shift/views.py
@@ -5,6 +5,56 @@ from .models import Shift, Worker
 from django.views.generic.base import TemplateView
 
 
+
+class ContextView(TemplateView):
+    context = {}
+    def get_context_data(self,**kwargs):
+        context = super().get_context_data(**kwargs)
+        if "user" in self.request.session:
+            context['logged'] = True
+            if Shift.objects.filter(worker=self.request.session.get('user'), end_time=None).count() == 0:
+                context['shift_started'] = False
+            else:
+                context['shift_started'] = True
+        return context
+
+class LoginView(ContextView):
+    template_name = 'shift/index.html'
+    def post(self, request):
+        user = models.Worker.objects.filter(login=request.POST.get('login'))
+        if len(user) == 0:
+            self.context['wrong_user'] = True
+        else:
+            if user[0].password == request.POST.get('password'):
+                request.session['user'] = user[0].pk
+                request.session['logged'] = True
+                return redirect('home')
+            else:
+                self.context['wrong_password'] = True
+
+class HomeView(ContextView):
+    template_name = 'shift/home.html'
+
+class UserStartShiftView(ContextView):
+    def get(self, request):
+        user_id = request.session.get('user')
+        new_shift = Shift(worker=Worker.objects.get(id=user_id))
+        new_shift.save()
+        return redirect(request.META['HTTP_REFERER'])
+
+class UserEndShiftView(ContextView):
+    def get(self, request):
+        user_id = request.session.get('user')
+        Shift.objects.filter(worker=Worker.objects.get(id=user_id), end_time=None).update(
+            end_time=datetime.datetime.now())
+        return redirect(request.META['HTTP_REFERER'])
+
+class LogOutView(ContextView):
+    def get(self, request):
+        request.session.flush()
+        self.context.clear()
+        return redirect('login')
+
 #def index(request):
     #from pdb import set_trace;set_trace()
 #    context = {}
@@ -26,45 +76,6 @@ from django.views.generic.base import TemplateView
          #   else:
           #      context['wrong_password'] = True
    # return render(request, 'shift/index.html', context=context)
-class ContextView(TemplateView):
-    context = {'logged': False}
-    def get_context_data(self, request, **kwargs):
-        context = super().get_context_data(**kwargs)
-        if "user" in request:
-            context['logged'] = True
-            if Shift.objects.filter(worker=request.get('user'), end_time=None).count() == 0:
-                context['shift_started'] = False
-            else:
-                context['shift_started'] = True
-        return context
-
-
-class LoginView(ContextView):
-    template_name = 'index.html'
-    def post(self, request):
-        user = models.Worker.objects.filter(login=request.POST.get('login'))
-        if len(user) == 0:
-            self.context['wrong_user'] = True
-        else:
-            if user[0].password == request.POST.get('password'):
-                request.session['user'] = user[0].pk
-                request.session['logged'] = True
-                return redirect('home')
-            else:
-                self.context['wrong_password'] = True
-
-class HomeView(ContextView):
-    def get(self, request):
-        if request.session['logged'] is True:
-            self.context['logged'] = True
-            if Shift.objects.filter(worker=request.session.get('user'), end_time=None).count() == 0:
-                self.context['shift_started'] = False
-            else:
-                self.context['shift_started'] = True
-        else:
-            return redirect('login')
-        return render(request, 'shift/home.html', context=self.context)
-
 
 #def home(request):
  #   context = {}
@@ -84,20 +95,6 @@ class HomeView(ContextView):
    # new_shift.save()
     #return redirect(request.META['HTTP_REFERER'])
 
-class UserStartShiftView(ContextView):
-    def get(self, request):
-        user_id = request.session.get('user')
-        new_shift = Shift(worker=Worker.objects.get(id=user_id))
-        new_shift.save()
-        return redirect(request.META['HTTP_REFERER'])
-
-class UserEndShiftView(ContextView):
-    def get(self, request):
-        user_id = request.session.get('user')
-        Shift.objects.filter(worker=Worker.objects.get(id=user_id), end_time=None).update(
-            end_time=datetime.datetime.now())
-        return redirect(request.META['HTTP_REFERER'])
-
 #def user_end_shift(request):
  #   user_id = request.session.get('user')
   #  Shift.objects.filter(worker=Worker.objects.get(id=user_id), end_time=None).update(end_time=datetime.datetime.now())
@@ -106,10 +103,3 @@ class UserEndShiftView(ContextView):
 #def log_out(request):
  #   request.session.flush()
   #  return redirect('login')
-
-class LogOutView(ContextView):
-    def get(self, request):
-        request.session.flush()
-        self.context.clear()
-        return redirect('login')
-
